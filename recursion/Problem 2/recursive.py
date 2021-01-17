7edfb6405169fc7d8e595b45fa5fe3c3b73f6799
Multivalence <47169252+Multivalence@users.noreply.github.com>
2021-01-16 23:23:03 -0500

    Update recursive.py

--- recursion/Problem 2/recursive.py
+++ recursion/Problem 2/recursive.py
@@ -1,29 +1,61 @@
 
+
+def substrings(word : str) -> list:
+
+	"""
+	Given a string, returns all palindromic sub-strings
+
+	Parameters
+	----------
+	word : str
+			String that may contain palindrome sub-strings
+
+	Returns
+	-------
+	list
+			all palindromic sub-strings found in word
+
+	"""
+
+	if len(word) == 0:
+		return [word]
+	
+	subs = []
+	subs.append(word)
+
+	subs += substrings(word[1:])
+	subs += substrings(word[:-1])
+	
+
+	return [i for i in set(subs) if len(i) > 1 and i == i[::-1] and i != ""]
+
+
+	
 def palindrome(word : str) -> int:
-    """
-    Given a string, calculates the amount of palindromes that exist within that string
 
-    Parameters
-    ----------
-    word : str
-       String that may contain palindrome sub-strings
+	"""
+	Given a string, calculates the amount of palindromes that exist within that string
 
-    Returns
-    -------
-    int
-       number of palindromes in string
+	Parameters
+	----------
+	word : str
+			String that may contain palindrome sub-strings
 
-    """
-    
-    word = word.lower()
-    
-    if len(word) in [2, 3]:
-        return 1 if word[0] == word[-1] else 0
+	Returns
+	-------
+	int
+			number of palindromes in string
 
-    return (1 if word[0] == word[-1] else 0) + palindrome(word[1:-1])
+	"""
 
+	return len(substrings(word))
+	
 
 if __name__ == '__main__':
-    print(palindrome('ada'))  # 1
-    print(palindrome('eadae'))  # 2
-    print(palindrome('ade')) # 0
+	print(palindrome('ada'))  # 1
+	print(palindrome('eadae'))  # 2
+	print(palindrome('ade')) # 0
+	print(palindrome("array")) #2
+
+
+	
